7c5807674edb6eafbcabc5d83535bae8a1bb1226
Marllon Drion <marllondrion@gmail.com>
2020-08-20 22:22:29 +1000

    Middleware
    how to user and

--- module01/index.js
+++ module01/index.js
@@ -4,8 +4,49 @@ const server = express();
 
 server.use(express.json());
 
-
+//List of products
 const products = ['Pen', 'Mobile Phone', 'TV']
+
+
+/**
+ * Middleware functions are functions that have access to 
+ * the request object (req), the response object (res), and 
+ * the next middleware function in the application’s 
+ * request-response cycle. These functions are used to 
+ * modify req and res objects for tasks like parsing 
+ * request bodies, adding response headers, etc.
+ */
+
+/**
+ * Global middleware
+ */
+server.use((req, res, next) => {
+    console.log(`${Date()} - method: ${req.method} - url: ${req.url} - body: ${JSON.stringify(req.JSON)}`);
+
+    next();
+
+    console.info("end")
+})
+
+/**
+ * writing-middleware
+ */
+function checkProductExists(req, res, next) {
+    if (!req.body.name) {
+        return res.status(400).json({ error: 'product not found on request body' });
+    }
+    return next();
+}
+
+function checkProductInArray(req, res, next) {
+    const product = products[req.params.index]
+    if (!product) {
+        return res.status(400).json({ error: 'product does not found in the DB' });
+    }
+    req.product = product;
+    return next();
+}
+
 
 
 /**
@@ -24,10 +65,13 @@ server.get('/getInfo', (req, res) => {
  */
 
 /*Routte params*/
-server.get('/product/:index', (req, res) => {
-    const { index } = req.params;
+server.get('/product/:index', checkProductInArray, (req, res) => {
+
     //return res.json(({ 'message': `Hi Mate your product id is ${id}` }));
-    return res.json(products[index]);
+    //before checkProductInArray: 
+    //const { index } = req.params;
+    //return res.json(products[index]);
+    return res.json(req.product);
 });
 
 server.get('/products/', (req, res) => {
@@ -36,7 +80,7 @@ server.get('/products/', (req, res) => {
 
 /*Request body*/
 //add a new product
-server.post('/product', (req, res) => {
+server.post('/product', checkProductExists, (req, res) => {
     const { name } = req.body;
     products.push(name);
     return res.json(products);
@@ -45,7 +89,7 @@ server.post('/product', (req, res) => {
 /**
  * Edit
  */
-server.put('/product/:index', (req, res) => {
+server.put('/product/:index', checkProductExists, checkProductInArray, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
     products[index] = name;
@@ -54,12 +98,16 @@ server.put('/product/:index', (req, res) => {
 /**
  * Delete product by the index
  */
-server.delete('/product/:index', (req, res) => {
-    const { index } = req.body;
-    products.pop(products[index]);
+server.delete('/product/:index', checkProductInArray, (req, res) => {
+    //Before checkProductInArray:
+    //const { index } = req.body;
+    //products.pop(products[index]);
     //products.splice(index,1);
+
+    products.pop(req.product);
     return res.send();
 });
+
 
 
 /**
