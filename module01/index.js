a23a1588bb490fddf8170efd264ff3250f3ad87c
Marllon Drion <marllondrion@gmail.com>
2020-08-20 21:01:03 +1000

    CRUD done
    I completed the CRUD, installation and configuration of the nodemon.

--- module01/index.js
+++ module01/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
 const server = express();
+
+server.use(express.json());
+
+
+const products = ['Pen', 'Mobile Phone', 'TV']
+
+
 /**
  * Query params = url?name={value}
  * Routte params = url/product/{value}
@@ -12,15 +19,48 @@ server.get('/getInfo', (req, res) => {
     const name = req.query.name;
     return res.json(({ 'message': `Hi Mate ${name}` }));
 });
+/**
+ * CRUD:
+ */
 
 /*Routte params*/
-server.get('/product/:id', (req, res) => {
-    const { id } = req.params;
-    return res.json(({ 'message': `Hi Mate your product id is ${id}` }));
+server.get('/product/:index', (req, res) => {
+    const { index } = req.params;
+    //return res.json(({ 'message': `Hi Mate your product id is ${id}` }));
+    return res.json(products[index]);
 });
 
+server.get('/products/', (req, res) => {
+    return res.json(products);
+});
 
 /*Request body*/
+//add a new product
+server.post('/product', (req, res) => {
+    const { name } = req.body;
+    products.push(name);
+    return res.json(products);
+});
+
+/**
+ * Edit
+ */
+server.put('/product/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    products[index] = name;
+    return res.json(products);
+});
+/**
+ * Delete product by the index
+ */
+server.delete('/product/:index', (req, res) => {
+    const { index } = req.body;
+    products.pop(products[index]);
+    //products.splice(index,1);
+    return res.send();
+});
+
 
 /**
  * Hello World
